7b8cfe0d19d17e0433460cab6f9c0b1f676dea5a
ksukriti08 <40966384+ksukriti08@users.noreply.github.com>
2025-02-26 17:33:58 -0500

    added one hot encoding for labels & loaded test data

--- classification.py
+++ classification.py
@@ -4,23 +4,48 @@ import pandas
 
 ENCODING = 512
 
-def softmax():
+def load_data():
     trainingData = pandas.read_csv("train.csv")
-    X = trainingData.to_numpy()[:, :-1]
-    candidateIds = X[:, 0]
-    print(candidateIds.shape)
-    n = candidateIds.shape[0]
-    coughs = np.zeros((n, ENCODING))
-    vowels = np.zeros((n, ENCODING))
-    for i in range(n):
-        if candidateIds[i] == "d7ed7deb786c3":
+    testingData = pandas.read_csv("test.csv")
+
+    Xtrain = trainingData.to_numpy()[:, :-1] # ignores labels in last column
+    Ytest = testingData.to_numpy()[:, :-1]
+    Xlabel = trainingData.to_numpy()[:, -1] # grabs labels from last column
+    Xlabel = Xlabel.reshape(Xlabel.shape[0],1) # makes shape a 2d array, easier for later
+    print("Num test candidates: ")
+    print(Ytest.shape[0])
+    candidateIds = Xtrain[:, 0]
+    print("Num train candidates: ")
+    print(candidateIds.shape[0])
+    num_train = candidateIds.shape[0]
+    num_test = Ytest.shape[0]
+    train_coughs = np.zeros((num_train, ENCODING))
+    test_coughs = np.zeros((num_test, ENCODING))
+    vowels = np.zeros((num_train, ENCODING))
+    onehot_train_labels = np.zeros((Xtrain.shape[0], 3)) # 3 classes to predict
+    onehot_train_labels[np.arange(Xtrain.shape[0]),Xlabel[:,0].astype(int)] = 1 # performs one hot encoding 
+
+    for i in range(num_train):
+        if candidateIds[i] == "d7ed7deb786c3": # ask ellys what this is
             continue
-        coughs[i] = np.load(f"sounds/sounds/{candidateIds[i]}/cough-opera.npy")
+        train_coughs[i] = np.load(f"sounds/sounds/{candidateIds[i]}/cough-opera.npy") # loads cough data for each participant
         # vowels[i] = np.load(f"sounds/sounds/{candidateIds[i]}/vowel-opera.npy")
         # print(candidateIds[i])
-    print(coughs.shape)
-    # X = np.append(coughs, np.append(vowels, X[:, 1:], axis=1), axis=1)
-    X = np.append(coughs, X[:, 1:], axis=1)
-    print(X.shape)
+    print("Shape of train coughs: ")
+    print(train_coughs.shape)
+    print("Shape of train labels (one-hot encoded): ")
+    print(onehot_train_labels.shape) # 3 classes for 3 diseases
 
-softmax()
+    for i in range(num_test):
+        test_coughs[i] = np.load(f"sounds/sounds/{candidateIds[i]}/cough-opera.npy") # loads cough data for test participants
+
+    print("Shape of test coughs: ")
+    print(test_coughs.shape)
+
+
+    # X = np.append(coughs, np.append(vowels, X[:, 1:], axis=1), axis=1)
+    Xtrain = np.append(train_coughs, Xtrain[:, 1:], axis=1) # adds coughs to Xtrain array
+    Ytest = np.append(test_coughs, Ytest[:, 1:], axis=1) # adds coughs to Ytest array
+
+load_data()
+
