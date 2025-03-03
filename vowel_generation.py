cf9b77385a88b080dcb90e34024c16c3ba6833af
taccat3 <tcaplan172@gmail.com>
2025-03-02 22:25:19 -0500

    finished vowel generation code, works well for npy, not great for json

--- vowel_generation.py
+++ vowel_generation.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas
+import json
 
 ENCODING = 512
 NUM_CLASSES = 3
 
-def load_vowel_data():
+def load_vowel_data(type="npy"):
     trainingData = pandas.read_csv("train.csv")
 
     # grab the training data from CSV
@@ -33,7 +34,12 @@ def load_vowel_data():
     testIndices = np.zeros(total_count)
     for i in range(total_count):
         try:
-            vowels[i] = np.load(f"sounds/sounds/{candidateIds[i]}/vowel-opera.npy") # loads vowel data for each participant
+            if type == "npy":
+                vowels[i] = np.load(f"sounds/sounds/{candidateIds[i]}/vowel-opera.npy") # loads vowel data for each participant
+            else:
+                with open(f"sounds/sounds/{candidateIds[i]}/emb_vowel.json") as f:
+                    temp = json.load(f)
+                vowels[i] = np.array(temp)
             trainIndices[i] = 1
         except FileNotFoundError:
             testIndices[i] = 1
@@ -55,7 +61,108 @@ def load_vowel_data():
 
     return Xtrain, Ytrain, Xtest
 
-Xtrain, Ytrain, Xtest = load_vowel_data()
+def relu (z):
+    return np.maximum(0, z)
 
-print(Xtrain.shape, Ytrain.shape, Xtest.shape)
+def fMSE(yhat, y):
+    n = yhat.shape[1]
+    return np.sum(np.sum((y - yhat) ** 2, axis=0) / (2 * n)) / n
 
+def forward_prop (x, y, W1, b1, W2, b2):
+    z = np.dot(W1, x) + b1.reshape(-1, 1)  # 20xbatchSize
+    h = relu(z) # 20xbatchSize
+    yhat = np.dot(W2, h) + b2.reshape(-1, 1)  # 1xbatchSize
+    loss = fMSE(yhat, y)
+    return loss, x, z, h, yhat
+
+def relu_prime(z):
+    return np.where(z > 0, 1, 0)
+
+def back_prop (X, y, W1, b1, W2, b2, alpha=0):
+    n = X.shape[1]
+    _, _, z, h, yhat = forward_prop(X, y, W1, b1, W2, b2)
+    g = (np.dot((yhat-y).T, W2) * relu_prime(z.T)).T # 20xbatchSize
+    gradW1 = np.dot(g, X.T) / n + alpha / n * W1 # 20x2304
+    gradW2 = np.dot((yhat-y), h.T) / n + alpha / n * W2 # 1x20
+    gradb1 = np.sum(g, axis=1) / n # 20 x batchSize
+    gradb2 = (np.sum(yhat-y, axis=1)) / n # 1xbatchSize
+    return gradW1, gradb1, gradW2, gradb2
+
+def train (trainX, trainY, W1, b1, W2, b2, testX, testY, epsilon = 1e-2, batchSize = 64, numEpochs = 1000, alpha=0):
+    M = trainX.shape[1] # number of examples
+    indexes = np.random.permutation(M)
+    for e in range(numEpochs):
+        if e % 20 == 0: print("Progress:", str(round(float(e)/numEpochs*100, 2)) + "%")
+        for i in range(0, M, batchSize):
+            end = i + batchSize if ((i+batchSize) < M) else M-1
+            batchX = trainX[:, indexes[i:end]] # 2304 x batchSize
+            batchY = trainY[:, indexes[i:end]] # 1 x batchSize
+            gradW1, gradb1, gradW2, gradb2 = back_prop(batchX, batchY, W1, b1, W2, b2, alpha)
+            curr_epsilon = epsilon if epsilon > 0 else (1e-4 if e < 20 else (1e-5 if e < 100 else 1e-6))
+            curr_epsilon = epsilon
+            W1 -= curr_epsilon * gradW1 
+            b1 -= curr_epsilon * gradb1
+            W2 -= curr_epsilon * gradW2
+            b2 -= curr_epsilon * gradb2
+
+    return W1, b1, W2, b2
+
+def vowelNN(trainX, trainY, testX, testY, epsilon=1e-4, batchSize=32, numEpochs=100, numHidden=20):
+    num_input = trainX.shape[0]
+    num_output = trainY.shape[0]
+    # Initialize weights to reasonable random values
+    W1 = 2*(np.random.random(size=(numHidden, num_input))/num_input**0.5) - 1./num_input**0.5
+    b1 = 0.01 * np.ones(numHidden)
+    W2 = 2*(np.random.random(size=(num_output, numHidden))/numHidden**0.5) - 1./numHidden**0.5
+    b2 = np.mean(trainY)
+
+    W1, b1, W2, b2 = train(trainX, trainY, W1, b1, W2, b2, testX, testY, epsilon=epsilon, batchSize=batchSize, numEpochs=numEpochs)
+    loss, _, _, _, _ = forward_prop(trainX, trainY, W1, b1, W2, b2)
+    print("Final Training Loss:", loss)
+    loss, _, _, _, _ = forward_prop(testX, testY, W1, b1, W2, b2)
+    print("Final Testing Loss:", loss)
+
+    return W1, b1, W2, b2
+
+def format_vowel_data(train_test_split=0.75, type="npy"):
+
+    allX, allY, actualTest = load_vowel_data(type)
+    print(allX.shape, allY.shape, actualTest.shape)
+
+    print(f"Shape of all training data before deleting: {allX.shape}")
+    allX = np.delete(allX, 519, axis=1) # deleting column 519
+
+    print(f"Shape of actual testing data before deleting: {actualTest.shape}")
+    actualTest = np.delete(actualTest, 519, axis=1)# deleted column 519 
+
+    cutoff = int(allX.shape[0] * train_test_split)
+
+    Xtrain = allX[0:cutoff, :]
+    Ytrain = allY[0:cutoff, :]
+    Xtest = allX[cutoff+1:, :]
+    Ytest = allY[cutoff+1:, :]
+
+    print(f"Training data shape: X={Xtrain.shape}, Y={Ytrain.shape}")
+    print(f"Testing data shape: X={Xtest.shape}, Y={Ytest.shape}")
+    print(f"Actual Testing data shape:{actualTest.shape}")
+
+    return Xtrain, Ytrain, Xtest, Ytest, actualTest
+
+def generate_vowels(X, W1, b1, W2, b2):
+    z = np.dot(W1, X.T) + b1.reshape(-1, 1)  # 20xbatchSize
+    h = relu(z) # 20xbatchSize
+    yhat = np.dot(W2, h) + b2.reshape(-1, 1)  # 1xbatchSize
+    return yhat.T
+
+if __name__ == "__main__":   
+
+    file_type = "json" 
+
+    Xtrain, Ytrain, Xtest, Ytest, actualTest = format_vowel_data(type=file_type)
+
+    if file_type == "npy":
+        W1, b1, W2, b2 = vowelNN(Xtrain.T, Ytrain.T, Xtest.T, Ytest.T, epsilon=0.00001, batchSize=16, numEpochs=200, numHidden=20)
+    else:
+        W1, b1, W2, b2 = vowelNN(Xtrain.T, Ytrain.T, Xtest.T, Ytest.T, epsilon=0.00001, batchSize=32, numEpochs=100, numHidden=20)
+
+    print(generate_vowels(actualTest, W1, b1, W2, b2).shape)
