a70f3f0fec83becdc254d4f0340a3566ae07f09b
Era Kalaja <ekalaja@wpi.edu>
2025-03-04 17:46:01 -0500

    data load update

--- classification.py
+++ classification.py
@@ -1,6 +1,7 @@
 import numpy as np
 # import scipy
 import pandas
+import json
 
 ENCODING = 512
 NUM_CLASSES = 3
@@ -8,31 +9,68 @@ NUM_CLASSES = 3
 def load_data():
     trainingData = pandas.read_csv("train.csv")
     testingData = pandas.read_csv("test.csv")
+
     Xtrain = trainingData.to_numpy()[:, :-1] # ignores labels in last column
-    Xtrainall = trainingData.to_numpy()[:,:]
     Xtest = testingData.to_numpy()
-    XtestIDs = Xtest[:,0]
+    XtestIDs = Xtest[:, 0]
     Ytrain = np.atleast_2d(trainingData.to_numpy()[:, -1]).T # grabs labels from last column
-    Ytrain = Ytrain.reshape(Ytrain.shape[0] , 1) # makes shape a 2d array, easier for later
+    Ytrain = Ytrain.reshape(Ytrain.shape[0], 1) # makes shape a 2d array, easier for later
+
     num_train = Xtrain.shape[0]
     num_test = Xtest.shape[0]
-    train_coughs = np.zeros((num_train, ENCODING))
-    test_coughs = np.zeros((num_test, ENCODING))
     onehot_train_labels = np.zeros((num_train, NUM_CLASSES)) # 3 classes to predict
     onehot_train_labels[np.arange(num_train), Ytrain[:, 0].astype(int)] = 1 # performs one hot encoding
 
+    onehot_train_coldpresent = np.zeros((num_train, 3))
+    onehot_train_coldpresent[Xtrain[:, 8] == 0, 0] = 1
+    onehot_train_coldpresent[Xtrain[:, 8] == 1, 1] = 1
+    onehot_train_coldpresent[np.isnan(Xtrain[:, 8].astype(float)), 2] = 1
+    onehot_test_coldpresent = np.zeros((num_test, 3))
+    onehot_test_coldpresent[Xtest[:, 8] == 0, 0] = 1
+    onehot_test_coldpresent[Xtest[:, 8] == 1, 1] = 1
+    onehot_test_coldpresent[np.isnan(Xtest[:, 8].astype(float)), 2] = 1
+
     candidateIds = Xtrain[:, 0]
+    Xtrain = np.concatenate((Xtrain[:, 1:8].astype(float), onehot_train_coldpresent, Xtrain[:, 9].reshape(num_train, 1).astype(float)), axis=1, dtype=float)
+
+    train_coughs = np.zeros((num_train, ENCODING))
+    test_coughs = np.zeros((num_test, ENCODING))
+    train_vowels = np.zeros((num_train, ENCODING))
+    test_vowels = np.zeros((num_test, ENCODING))
+    newXtrain = np.zeros((0, 523))
+    new_onehot_train_labels = np.zeros((0, 3))
     for i in range(num_train):
-        train_coughs[i] = np.load(f"sounds/sounds/{candidateIds[i]}/cough-opera.npy") # loads cough data for each participant
+        try:
+            with open(f"sounds/sounds/{candidateIds[i]}/emb_cough.json") as f:
+                coughs = np.append(np.array(json.load(f)), np.load(f"sounds/sounds/{candidateIds[i]}/cough-opera.npy"), axis=0)
+                newXtrain = np.concatenate((newXtrain, np.concatenate((coughs, np.tile(Xtrain[i], (coughs.shape[0], 1))), axis=1)), axis=0)
+                new_onehot_train_labels = np.append(new_onehot_train_labels, np.tile(onehot_train_labels[i], (coughs.shape[0], 1)), axis=0)
+        except:
+            continue
 
+    print()
     candidateIds = Xtest[:, 0]
+    Xtest = np.concatenate((Xtest[:, 1:8].astype(float), onehot_test_coldpresent, Xtest[:, 9].reshape(num_test, 1).astype(float)), axis=1, dtype=float)
+    newXtest = np.zeros((0, 523))
     for i in range(num_test):
-        test_coughs[i] = np.load(f"sounds/sounds/{candidateIds[i]}/cough-opera.npy") # loads cough data for test participants
+        try:
+            with open(f"sounds/sounds/{candidateIds[i]}/emb_cough.json") as f:
+                coughs = np.array(json.load(f))
+                newXtest = np.append(newXtest, np.concatenate((coughs.mean(axis=1), Xtest[i]), axis=1), axis=0)
+        except:
+            cough = np.load(f"sounds/sounds/{candidateIds[i]}/cough-opera.npy")
+            newXtest = np.append(newXtest, np.concatenate((cough, Xtest[i].reshape((1, Xtest.shape[1]))), axis=1), axis=0)
 
-    Xtrain = np.append(train_coughs, Xtrain[:, 1:].astype(float), axis=1) # adds coughs to Xtrain array
-    Xtrainall = np.append(train_coughs, Xtrainall[:, 1:].astype(float), axis=1) # adds coughs to Xtrain array
-    Xtest = np.append(test_coughs, Xtest[:, 1:].astype(float), axis=1) # adds coughs to Ytest array
-    return Xtrain, onehot_train_labels, Xtest, XtestIDs, Xtrainall 
+    cough_noise = np.random.default_rng().normal(0, 1e-1, (newXtrain.shape[0], ENCODING))
+    age_noise = np.random.default_rng().normal(0, 1, newXtrain[:, ENCODING].shape)
+    newXtrain = np.vstack((newXtrain, np.concatenate((newXtrain[:, :ENCODING] + cough_noise, np.atleast_2d(newXtrain[:, ENCODING] + age_noise).T, newXtrain[:, ENCODING + 1:]), axis=1)))
 
-Xtrain, onehot_train_labels, Xtest, XtestIDs, Xtrainall = load_data()
-# print(Xtrainall.shape)
+    new_onehot_train_labels = np.tile(new_onehot_train_labels, (2, 1))
+    print()
+    print(newXtrain.shape)
+    print(newXtest.shape)
+    print(new_onehot_train_labels.shape)
+
+    return newXtrain, new_onehot_train_labels, newXtest, XtestIDs
+
+load_data()
